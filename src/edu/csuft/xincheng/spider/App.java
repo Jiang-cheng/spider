68a2622d460770368ab3d37b468fa1d6f23de2d0
Jiang-cheng <1575240341@qq.com>
2018-12-16 17:21:43 +0800

    解析影片信息

--- src/edu/csuft/xincheng/spider/App.java
+++ src/edu/csuft/xincheng/spider/App.java
@@ -1,10 +1,55 @@
 package edu.csuft.xincheng.spider;
 
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 /**
-    * 程序的入口
+ *@程序的入口
  * @author xincheng
  * @author other
  */
 public class App {
 
+//alt+/
+	public static void main(String[] args)  {
+	
+		
+		//目标 URL
+		String url="https://movie.douban.com/top250";
+		//使用JSOUP抓取文档
+		try {
+			Document doc=Jsoup.connect(url).get();
+			
+			Elements es=doc.select(".grid_view .item");
+			System.out.println(es.size());
+			
+			for(Element e:es)
+			{
+				//每一部影片
+				Element t=e.select(".title").first();
+				String x=e.select(".star span").last().text();
+				System.out.println(t.text()+","+x);
+			}
+			
+			/*String title=doc.title();
+		    String html=doc.html();
+			String data=doc.data();
+			
+		    System.out.println(title);
+			System.out.println(data);*/
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+		
+		
+		
+	}
 }
