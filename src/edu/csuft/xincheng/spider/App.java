63cf23e4285cd529651ab2aab3893c9e891296ab
Jiang-cheng <1575240341@qq.com>
2018-12-16 17:25:56 +0800

    解析影片信息

--- src/edu/csuft/xincheng/spider/App.java
+++ src/edu/csuft/xincheng/spider/App.java
@@ -1,11 +1,13 @@
 package edu.csuft.xincheng.spider;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+
 
 /**
  *@程序的入口
@@ -27,12 +29,19 @@ public class App {
 			Elements es=doc.select(".grid_view .item");
 			System.out.println(es.size());
 			
+			//创建一个影片的列表
+			ArrayList<Film> list=new ArrayList<>();
 			for(Element e:es)
 			{
+				Film f=new Film();
 				//每一部影片
 				Element t=e.select(".title").first();
 				String x=e.select(".star span").last().text();
 				System.out.println(t.text()+","+x);
+				
+				//f.id
+				//f.title
+				list.add(f);			
 			}
 			
 			/*String title=doc.title();
